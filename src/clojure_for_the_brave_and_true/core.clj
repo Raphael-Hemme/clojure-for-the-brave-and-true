8894885b580334ee0bf3148601bb7886919b76e2
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-05 22:53:35 +0200

    Copy code from yesterday after having to delete the repo for wrong app structure, git setup etc

--- src/clojure_for_the_brave_and_true/core.clj
+++ src/clojure_for_the_brave_and_true/core.clj
@@ -1,7 +1,56 @@
 (ns clojure-for-the-brave-and-true.core
   (:gen-class))
 
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  [part]
+  ;; (println part)
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and a :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts (set [part (matching-part part)])))))))
+
+(defn own-symmetrize-b-p ;; It's not sorted as in the original example but at least each map is present only once inside the result list
+  "use mapping"
+  [asym-body-parts]
+  (println (set (concat (map matching-part asym-body-parts) (apply list asym-body-parts)))))
+
+(defn better-symmetrizer-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
