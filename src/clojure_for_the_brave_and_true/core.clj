6782126e894bdb6cac46d8a770f94347c2f05a0e
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-05 23:09:08 +0200

    Manually copy the hit function

--- src/clojure_for_the_brave_and_true/core.clj
+++ src/clojure_for_the_brave_and_true/core.clj
@@ -47,10 +47,21 @@
   [asym-body-parts]
   (println (set (concat (map matching-part asym-body-parts) (apply list asym-body-parts)))))
 
-(defn better-symmetrizer-body-parts
+(defn better-symmetrize-body-parts
   "Expects a seq of maps that have a :name and :size"
   [asym-body-parts]
   (reduce (fn [final-body-parts part]
             (into final-body-parts (set [part (matching-part part)])))
           []
           asym-body-parts))
+
+(defn hit 
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+      accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
