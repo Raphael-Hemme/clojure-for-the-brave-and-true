0c07393e366f6adf3b31f63e7782cf6ba367368e
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-14 20:30:04 +0200

    Add the listing for the divine-cheese-code.visualization.svg but rename it so it fits with the current naming scheme

---
+++ chap-06/src/chap_06/visualization/svg.clj
@@ -0,0 +1,10 @@
+(ns chap-06.visualization.svg)
+
+(defn latlng->point
+  "Convert lat/lng map to comma-separated string"
+  [latlng]
+  (str (:lng latlng) "," (:lat latlng)))
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
