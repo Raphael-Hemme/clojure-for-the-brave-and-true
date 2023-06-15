cb09b83f9462c45a280e424f48ad4dd7577d4836
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-15 22:55:22 +0200

    Add first parts of chapter 6 listings

--- chap-06/src/chap_06/core.clj
+++ chap-06/src/chap_06/core.clj
@@ -1,7 +1,30 @@
-(ns chap-06.core
-  (:gen-class))
+(ns chap-06.core)
+(require '[chap-06.visualization.svg :as svg])
+(refer 'chap-06.visualization.svg)
 
-(defn -main
-  "I don't do a whole lot ... yet."
+
+(def heist [{:location "Cologne, Germany"
+             :cheese-name "Archbishop Hildebold's Cheese Pretzel"
+             :lat 50.95
+             :lng 6.97} 
+            {:location "Zurich, Switzerland"
+             :cheese-name "The Standard Emmental"
+             :lat 47.37
+             :lng 8.55} 
+            {:location "Marseille, France"
+             :cheese-name "Le Fromage de Cosquer"
+             :lat 43.30
+             :lng 5.37} 
+            {:location "Zurich, Switzerland"
+             :cheese-name "The Lesser Emmental"
+             :lat 47.37
+             :lng 8.55} 
+            {:location "Zurich, Switzerland" 
+             :cheese-name "The Standard Emmental" 
+             :lat 41.90 
+             :lng 12.45}
+            ])
+
+(defn -main 
   [& args]
-  (println "Hello, World!"))
+  (println (svg/points heist)))
