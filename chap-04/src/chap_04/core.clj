03326682a5ee3e5a6aa85d375bbcfd4e9ac4c2b4
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-17 20:06:05 +0200

    Finish main part of chap-004 excercise

--- chap-04/src/chap_04/core.clj
+++ chap-04/src/chap_04/core.clj
@@ -22,3 +22,23 @@
   [string]
   (map #(clojure.string/split % #",")
        (clojure.string/split string #"\n")))
+
+(defn mapify
+  "Return a seq of maps like: {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(defn only-names
+  [filter-result]
+  (map :name filter-result))
+
+(only-names (glitter-filter 3 (mapify (parse (slurp filename)))))
