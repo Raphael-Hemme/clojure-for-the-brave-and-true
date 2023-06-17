8d9cc6d7f96c15bb9fbb48c624cb6755d13f8890
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-17 18:39:58 +0200

    Add a few of the first funcitons of chap-04

--- chap-04/src/chap_04/core.clj
+++ chap-04/src/chap_04/core.clj
@@ -1,7 +1,24 @@
-(ns chap-04.core
-  (:gen-class))
+(ns chap-04.core)
+(def filename "suspects.csv")
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(slurp filename)
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
