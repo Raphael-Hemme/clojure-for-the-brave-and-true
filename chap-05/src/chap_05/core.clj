b3f29d51d102411d57ca0c3d8c856be75fd2aa44
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-06-22 08:36:38 +0200

    Add listings up until and including page 113

--- chap-05/src/chap_05/core.clj
+++ chap-05/src/chap_05/core.clj
@@ -30,6 +30,24 @@
    (let [new-sum (+ sum n)]
      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
 
+(def tri (tri*))
+
+(defn triangular?
+  "Tests if the number is triangular: e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns row number the position belongs to: pos 1 in row 1,
+   pos 2 and 3 in row 2 etc."
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
